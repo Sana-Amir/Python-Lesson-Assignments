002c10786a026931957097a14cf7d20232cb6c46
sanaa <sanaamir4274@gmail.com>
2021-11-10 01:52:43 +0100

    commit (Lesson 5, Task 1)

---
+++ Lesson 5 (Task 1).py
@@ -0,0 +1,14 @@
+import random
+randomlist = random.sample(range(10, 100), 10)
+print("Random list: ", randomlist)
+Max =  max(randomlist)
+
+class Break:
+    pass
+
+while True:
+    print("Greatest number: ", Max)
+    break
+
+
+
