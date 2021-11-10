0d463fedc5c8fab67f1b8cacb17f5e4273ffaee6
sanaa <sanaamir4274@gmail.com>
2021-11-10 19:44:39 +0100

    commit upgraded

--- Lesson 4 (Task 3).py
+++ Lesson 4 (Task 3).py
@@ -1,12 +1,7 @@
-word = input("Please enter a word:")
-list_of_letters = list(word)
-print(list_of_letters)
-
 import random
-input_string = "hello"
-length_input_string = len(input_string)
+x= input("Word: ")
 i = 1
-while i <= length_input_string:
-    print(random.choice(input_string), end = '')
-    i += 1
-
+while i<=5:
+    result = random.choice(x) + random.choice(x) + random.choice(x) + random.choice(x) + random.choice(x)
+    print(result)
+    i+=1
