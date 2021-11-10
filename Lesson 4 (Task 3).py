a5915b2add6661b60e8fa82e33b53a2117bafccd
sanaa <sanaamir4274@gmail.com>
2021-11-10 17:44:58 +0100

    commit

--- Lesson 4 (Task 3).py
+++ Lesson 4 (Task 3).py
@@ -1,3 +1,12 @@
 word = input("Please enter a word:")
 list_of_letters = list(word)
 print(list_of_letters)
+
+import random
+input_string = "hello"
+length_input_string = len(input_string)
+i = 1
+while i <= length_input_string:
+    print(random.choice(input_string), end = '')
+    i += 1
+
---
+++ Lesson 5 (Task 3).py
@@ -0,0 +1,8 @@
+
+list = list(range(1, 100))
+print("List: ", list)
+
+for i in range(0,100):
+  while True:
+    if (i% 7 == 0) and (i % 5!= 0):
+        print("List x: ", i)
