3718030fc3ed31d4f9881ba24afb679bbcfebe6b
sanaa <sanaamir4274@gmail.com>
2021-12-05 02:30:36 +0100

    Commit!

---
+++ Lesson 11 (Task 3).py
@@ -0,0 +1,90 @@
+class Product:
+    def __init__(self, type_, name, price):
+        self.type = type_
+        self.name = name
+        self.price = price
+
+
+class ProductStore:
+    def __init__(self):
+        self.products = []
+        self.income = 0
+
+    def add(self, product, amount):
+        self.products.append({
+            "product": product,
+            "amount": amount
+        })
+
+    def set_discount(self, identifier, percent, identifier_type='name'):
+        pass
+
+    def sell_product(self, product_name, amount):
+        for p in self.products:
+            product_in_store = p["product"]
+            amount_in_store = p["amount"]
+
+            if product_in_store.name != product_name:
+                continue
+
+            if amount_in_store < amount:
+                raise ValueError(f"Can't sell {amount} product(s). Store contains only {amount_in_store}.")
+
+            p['amount'] -= amount
+
+            self.income += product_in_store.price * amount
+
+    def get_income(self):
+        return self.income
+
+    def get_all_products(self):
+        for product in self.products:
+            product_in_store = product["product"]
+            amount_in_store = product["amount"]
+
+            print("{}\t | {}\t | ${}\t | {}".format(
+                product_in_store.type,
+                product_in_store.name,
+                product_in_store.price,
+                amount_in_store if amount_in_store > 0 else "Not in store"
+            ))
+
+    def get_product_info(self, product_name):
+        for p in self.products:
+            product_in_store = p["product"]
+            amount_in_store = p["amount"]
+
+            if product_in_store.name != product_name:
+                continue
+
+            return (product_in_store.name, amount_in_store)
+
+        raise ValueError("Product not found")
+
+
+def main():
+    store = ProductStore()
+
+    p1 = Product("Clothing", "Blazer", 10000)
+    p2 = Product("Shoes", "Stiletto", 5000)
+
+    store.add(p1, 10)
+    store.add(p2, 100)
+
+    print(" ")
+    print("In Store :")
+    print(" ")
+    store.get_all_products()
+
+    print("Selling 10 Stiletto...")
+    store.sell_product("Stiletto", 10)
+
+    print("Store state after selling:")
+    store.get_all_products()
+
+    print(" ")
+    print("Total income: ${}".format(store.get_income()))
+
+
+if __name__ == "__main__":
+    main()
